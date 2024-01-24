0a876fc7f10283d55bad94eb6cdb7fa4e613b671
AugmentVista <AuAgments@gmail.com>
2024-01-24 14:36:30 -0400

    added some supplementary classes

---
+++ Anti_Stupid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Playable
+{
+    // Use this example to determine if there is more than one instance of a class when there should not be.
+    internal class Map
+    {
+        private static int count;
+
+        public Map() 
+        { 
+        Debug.Assert(count == 0);
+        
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Playable
+{
+
+
+    // F12 on anything shows you where it comes from
+    internal class Settings
+    {
+        public const int StartingHealth = 100;
+        public const int StartingLevel = 1;
+
+
+
+    }
+}
