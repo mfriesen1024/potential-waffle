8ff32bbf89699119779ad5b459107ef167abb0b9
AugmentVista <AuAgments@gmail.com>
2024-02-01 20:35:56 -0400

    Enemy progress
    Enemy1 inherits from EnemyEntity which passes it player and mapdata

---
+++ Enemy1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Playable
+{
+    internal class Enemy1 : EnemyEntity
+    {
+        // Constructor for Enemy1, it needs to pass mapData and player to the base class constructor
+        public Enemy1(MapData mapData, Player player) : base(mapData, player)
+        {
+            // Enemy 1 specific initializations go here so that methods within Enemy1 can see them.  
+        }
+
+
+
+    }
+}
---
+++ EnemyEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Playable
+{
+    internal class EnemyEntity
+    {
+        private MapData mapData;
+        private Player player;
+        private int MapWidth;
+        private int MapHeight;
+
+        public EnemyEntity(MapData mapData, Player player)
+        {
+            this.mapData = mapData;
+            this.player = player; 
+        }
+        void MethodOrSomething()
+        {
+            int MapWidth = mapData.map.GetLength(1);
+            int MapHeight = mapData.map.GetLength(0);
+        }
+
+       
+
+
+
+    }
+}
