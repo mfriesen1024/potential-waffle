10a9a399ecffb72ef24d46645e493188c86118bf
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-09-24 10:40:11 -0300

    Rename classes to make more sense.

---
+++ Entry.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Design;
+using System.Text;
+
+namespace untitled
+{
+    public class Entry
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            GameManager.Initialize();
+            GameManager.InitializeEnemies();
+            MenuManager.DisplayMenu();
+            GameManager.RunGameLoop();
+        }
+    }
+}
