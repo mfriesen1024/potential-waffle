a0cdb904a1135fb7dade11df8e805f818380c0e4
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-01 14:22:30 -0300

    Create Utils.cs

---
+++ Utils.cs
@@ -0,0 +1,18 @@
+﻿namespace untitled
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Print an object.
+        /// </summary>
+        /// <param name="content">What to print.</param>
+        /// <param name="waitWhenDone">Waits for a readkey before continuing.</param>
+        static void Print(object content, bool waitWhenDone = false)
+        {
+            if(content is string)  Console.WriteLine(content as string); 
+            else Console.WriteLine(content.ToString());
+
+            if(waitWhenDone) Console.ReadKey(true);
+        }
+    }
+}
