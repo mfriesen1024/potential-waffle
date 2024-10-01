6650b64e9117980a4c580d969559b16b6e8d1402
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-01 10:32:56 -0300

    Add char to tile converter

--- Map/Tile.cs
+++ Map/Tile.cs
@@ -34,5 +34,67 @@ namespace untitled.Map
                 return true;
             }
         }
+
+        // We'll use this to convert from settings things
+        public static explicit operator Tile(char c)
+        {
+            Tile t = new();
+            switch (c)
+            {
+                case '╭':
+                case '─':
+                case '╮':
+                case '╯':
+                case '╰':
+                case '│':
+                case '┘':
+                case '┌':
+                case '┐':
+                case '└':
+                case '├':
+                case '┤':
+                case '┬':
+                case '┴':
+                    t.foreground = ConsoleColor.DarkMagenta;
+                    t.hazard = 1; // 1 means its a wall.
+                    break;
+                case '☻':
+                    t.foreground = ConsoleColor.Yellow;
+                    break;
+                case '╳':
+                    t.foreground = ConsoleColor.DarkGray;
+                    break;
+                case Settings.DuckChar:
+                case Settings.GooseChar:
+                case Settings.LionChar:
+                    t.foreground = ConsoleColor.Green;
+                    break;
+                case '░':
+                case '╦':
+                case '╠':
+                case '╣':
+                case '╩':
+                case '╬':
+                case '═':
+                    t.foreground = ConsoleColor.White; // white means they will be replaced with blanks when certain keys are obtained
+                    t.hazard = 1; // 1 means its a wall.
+                    break;
+                case 'H':
+                case 'A':
+                    t.foreground = ConsoleColor.Red;
+                    break;
+                case Settings.key0c:
+                case Settings.key1c:
+                case Settings.key2c:
+                case Settings.key3c:
+                case Settings.key4c:
+                case Settings.key5c:
+                case Settings.key6c:
+                    t.foreground = ConsoleColor.Cyan;
+                    break;
+            }
+
+            return t;
+        }
     }
 }
