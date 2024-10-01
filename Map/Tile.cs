2a4138b94d3f87314f99e56e24c7f69966dbc418
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-01 09:24:42 -0300

    Implement cloning

--- Map/Tile.cs
+++ Map/Tile.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using untitled.Interfaces;
 
 namespace untitled.Map
 {
-    internal class Tile
+    internal class Tile : ICustomCloneable<Tile>
     {
         public char character = ' ';
         public ConsoleColor foreground = ConsoleColor.White, background = ConsoleColor.Black;
         public int hazard = 0;
+
+        public Tile Clone()
+        {
+            return new Tile() { character = character, background = background, foreground = foreground, hazard = hazard };
+        }
     }
 }
