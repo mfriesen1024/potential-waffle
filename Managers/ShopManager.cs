86fe06aa13fa08644694247447cc393d35c99cdf
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-01 17:06:09 -0300

    Create some basic shop stuff.

---
+++ Managers/ShopManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace untitled.Managers
+{
+    /// <summary>
+    /// Manages shops for inventory purposes.
+    /// </summary>
+    internal class ShopManager
+    {
+    }
+}
