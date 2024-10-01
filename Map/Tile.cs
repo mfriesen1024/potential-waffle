a51d4c72d6123fa1a3c07e31c1d1fdbb96264af8
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-01 09:10:02 -0300

    Create Tile.cs

---
+++ Map/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace untitled.Map
+{
+    internal class Tile
+    {
+        public char character = ' ';
+        public ConsoleColor foreground = ConsoleColor.White, background = ConsoleColor.Black;
+        public int hazard = 0;
+    }
+}
