f2df25abf9f897db9b68fddfb63a83cddca8297e
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-07 19:19:59 -0300

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -5,25 +5,33 @@ namespace untitled
     public static class Utils
     {
         /// <summary>
+        /// Can be activated to forcibly disable the print function so it isn't visible in a release build.
+        /// </summary>
+        public static bool allowPrinting = false;
+
+        /// <summary>
         /// Print an object.
         /// </summary>
         /// <param name="content">What to print.</param>
         /// <param name="waitWhenDone">Waits for a readkey before continuing.</param>
         public static void Print(object content, bool waitWhenDone = false)
         {
-            Console.SetCursorPosition(0, 0);
-            if(content is string)
+            if(allowPrinting)
             {
-                Console.Write(content as string);
-                Debug.WriteLine(content as string);
-            }
-            else
-            {
-                Console.Write(content.ToString());
-                Debug.WriteLine(content.ToString());
-            }
+                Console.SetCursorPosition(0, 0);
+                if (content is string)
+                {
+                    Console.Write(content as string);
+                    Debug.WriteLine(content as string);
+                }
+                else
+                {
+                    Console.Write(content.ToString());
+                    Debug.WriteLine(content.ToString());
+                }
 
-            if(waitWhenDone) Console.ReadKey(true);
+                if (waitWhenDone) Console.ReadKey(true);
+            }
         }
     }
 }
